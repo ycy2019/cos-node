6d0e1239ae7b9d528d92a77ebfe94bc14d83687e
YC <774139813@qq.com>
2021-12-09 21:21:51 +0800

    upload

--- public/javascripts/cos.js
+++ public/javascripts/cos.js
@@ -1,7 +1,7 @@
 var COS = require('cos-nodejs-sdk-v5');
 var cos = new COS({
-    SecretId: 'AKIDs3fBbrL42aXRL3iarOhjeNrl6BVZLl23',
-    SecretKey: 'DrE4TcUAtNa5a9UaQH6sA7i4A4by4461'
+    SecretId: process.env.SECRETID || 'AKIDs3fBbrL42aXRL3iarOhjeNrl6BVZLl23',
+    SecretKey: process.env.SECRETKEY || 'DrE4TcUAtNa5a9UaQH6sA7i4A4by4461'
 })
 
 module.exports = cos;
