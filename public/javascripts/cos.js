860f86bd1be5c3aab14982ab7a2100480ba1b15e
774139813@qq.com <yangchen>
2021-12-25 04:05:35 +0800

    引入cos-yc模块，将cos的接口逻辑都放到cos-yc中更新

--- public/javascripts/cos.js
+++ public/javascripts/cos.js
@@ -1,7 +1,9 @@
-var COS = require('cos-nodejs-sdk-v5');
-var cos = new COS({
-    SecretId: process.env.SECRETID || 'AKIDs3fBbrL42aXRL3iarOhjeNrl6BVZLl23',
-    SecretKey: process.env.SECRETKEY || 'DrE4TcUAtNa5a9UaQH6sA7i4A4by4461'
-})
+const COS = require("cos-yc");
 
+const cos = new COS({
+    "SecretId": process.env.SECRETID || 'AKIDs3fBbrL42aXRL3iarOhjeNrl6BVZLl23',
+    "SecretKey": process.env.SECRETKEY || 'DrE4TcUAtNa5a9UaQH6sA7i4A4by4461',
+    "Bucket": "yc-1302181823",
+    "Region": "ap-shanghai"
+})
 module.exports = cos;
